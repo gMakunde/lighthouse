83b7d75d57a00412f8ea7e421d790b86c4b37c06
Paul Irish <paulirish@google.com>
2020-08-21 13:52:00 -0700

    forms config

--- lighthouse-cli/test/smokehouse/test-definitions/forms/form-config.js
+++ lighthouse-cli/test/smokehouse/test-definitions/forms/form-config.js
@@ -5,12 +5,14 @@
  */
 'use strict';
 
+const experimentalConfig = require('../../../../../lighthouse-core/config/experimental-config.js');
+
 /**
  * @type {LH.Config.Json}
  * Config file for running form gatherer  tests.
  */
 const config = {
-  extends: 'lighthouse:default',
+  ...experimentalConfig,
   settings: {
     onlyAudits: [
       'autocomplete',
